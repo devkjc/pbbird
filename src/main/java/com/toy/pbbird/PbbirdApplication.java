2b89fb854b4216214541d89e4f9cde5226e83eec
JongChan <kjc3590@songareeit.com>
2021-10-19 14:58:18 +0900

    [Bird/PostBox] init

---
+++ src/main/java/com/toy/pbbird/PbbirdApplication.java
@@ -0,0 +1,13 @@
+package com.toy.pbbird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbbirdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PbbirdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toy/pbbird/PbbirdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.pbbird;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbbirdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
